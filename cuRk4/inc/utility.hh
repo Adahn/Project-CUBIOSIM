28697540d8dbc7056d0a5a4fe72f6547a1acc069
Lgaudelet <lucasgaudelet@gmail.com>
2018-01-10 12:37:42 +0100

    repr modified to work with gpu

--- cuRk4/inc/utility.hh
+++ cuRk4/inc/utility.hh
@@ -5,28 +5,19 @@
 template<class state_type>
 __global__ void sumk(int n, state_type* out, state_type* v, state_type* coef, int n_v) 
 {
-	/*0if(threadIdx.x==0) {
-		printf("\n\t-- sumk --\n");
-		printf("%d coefs: ", n_v);
-		for(int i=0; i<n_v; i++) {
-			printf("%f\t", coef[i]);
-		}
-		printf("\n");
-	}
-	__syncthreads();*/
 
 	int grain = ceil((double)n/(gridDim.x*blockDim.x));
-	int index = blockIdx.x*blockDim.x*grain+threadIdx.x;
+	int tid = blockIdx.x*blockDim.x*grain+threadIdx.x;
 
 	for(int i=0; i<grain; i++) {
-		if(index<n) {
-			out[index] = 0;
+		if(tid<n) {
+			out[tid] = 0;
 			for(int j=0; j<n_v; j++) {
-				out[index]+=coef[j]*v[j*n+index];
+				out[tid]+=coef[j]*v[j*n+tid];
 			}
-			//printf("out[%d] = %f\n", index, out[index]);
+			//printf("out[%d] = %f\n", tid, out[tid]);
 		}
-		index+=blockDim.x;
+		tid+=blockDim.x;
 		
 	}
 
