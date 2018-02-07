653093d4e8dfd2616d1902ede974795e1bc455a6
Lgaudelet <lucasgaudelet@gmail.com>
2018-02-07 11:29:08 +0100

    comments and cleanup on utility.h

--- rk4/inc/utility.hh
+++ rk4/inc/utility.hh
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string>
 
+// prints out message followed by vector v
 template<class state_type>
-void debug_GPU(state_type* v, int dim, std::string message){
+void print_array(state_type* v, int dim, std::string message){
 
-	std::cout << message << '\n';
+	std::cout << message << endl;
 	for (int i = 0; i < dim; i++) {
 		std::cout << v[i] << "\t";
 	}
-	std::cout << '\n';
+	std::cout << endl;
 
 }
