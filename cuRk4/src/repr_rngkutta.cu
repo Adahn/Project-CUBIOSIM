54b96463028b5d34a6b912b82acad402b9562cbf
adrianAhne <adrian-ahne1@web.de>
2018-02-15 12:11:37 +0100

    added REAdme in cuRK4 and added comments

--- cuRk4/src/repr_rngkutta.cu
+++ cuRk4/src/repr_rngkutta.cu
@@ -9,6 +9,11 @@ authors:
 	Adrian Ahne
 	Lucas Gaudelet
 
+Comments of this forme:
+// !! this is a sample comment !!
+show where you can modify parameters, files, etc.
+
+
 ****************/
 
 // standard C++
@@ -31,6 +36,7 @@ authors:
 using namespace std;
 
 // Constants
+// !! Change parameter here if necessary !!
 const static int DEFAULT_N = 3;
 
 const static double Ktl = 1e-2;	// Translation rate
@@ -52,32 +58,41 @@ int main(int argc, char **argv) {
 	if(!help) help = chCommandLineGetBool("help", argc, argv);
 	if(help)  print_help(argv[0]);
 
-	// repressilator size
+	// user gets asked in command line to give a repressilator size
 	int n = -1;
 	chCommandLineGet<int>(&n, "n", argc, argv);
 	chCommandLineGet<int>(&n, "repressor-number", argc, argv);
 	n = (n!=-1)? n:DEFAULT_N;
-	
+
 	if( n%2==0 ) {
 		cout << "n must be an odd interger" << endl;
 		exit(-1);
 	}
 	else {
-		cout << "repressors : " << n 
+		cout << "repressors : " << n
 			<< "\tspecies: " << 2*(n+1)
 			<< "\treactions: " << 2*n+1 << endl;
 	}
 
 	// repressilator initialisation
+	// repr_rk4 : solver runge-kutta 4
+	// !! Change if necessary output file of results !!
 	Repressilator_ODE repr_rk4(n, dprot, dmRNA, Ktl, Ktr, KR, nR, "bin/result_rk4.csv");
 
 	// initial point
+	// !! Change if necessary initial value of Y0 !!
 	double* Y0 = (double*)calloc(2*(n+1), sizeof(double));	Y0[0] = 1;
 
 	// compute
+	// !! Change parameters of solver if necessary !!
+	int dim = 2*(n+1); // Number of species (size of input vector)
+	double t0 = 0.0; // Start time of simulation
+	double t_max = 1000; // End time of simulation
+	double step = 1e-2; // Step size
+	
 	cout << "call rk4...\t" << flush;
 	rk4_wrapper<double, Repressilator_ODE&>
-			( 2*(n+1), repr_rk4, Y0 , 0.0 , 1000, 1e-2);
+			( dim, repr_rk4, Y0 , t0 , t_max, step);
 	cout << "done" << endl;
 
 	free(Y0);
